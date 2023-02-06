399749ccfac511645e19b6a8b3f403054fa4995e
Alvaro Alonso <airuritac@gmail.com>
2023-02-06 10:57:05 +0100

    starting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import AuthenticationButton from './Components/auth0/AuthenticationButton';
-import ExternalApi from './views/external-api';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <AuthenticationButton />
-          <div>
-          <hr />
-            <ExternalApi />
-          </div>
-      </header>
-    </div>
+    <>
+
+      
+
+    </>
   );
 }
 
